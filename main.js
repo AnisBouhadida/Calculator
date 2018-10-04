da4acf54f9c80c1715c33e23b5e81b1710cb9db0
Anis Bouhadida <AB.geekanis@outlook.fr>
2018-10-04 18:04:03 +0200

    Finish simple calculator -- Need some Fix

--- main.js
+++ main.js
@@ -1,79 +1,134 @@
+// basic math functions : 
 function add(num1, num2) {
-      console.log(num1 + num2);
+      return result = num1 + num2;
 }
 
 function subtract(num1, num2) {
-      console.log(num1 - num2);
+      return result = num1 - num2;
 }
 
 function multiply(num1, num2) {
-      console.log(num1 * num2);
+      return result = num1 * num2;
 }
 
 function divide(num1, num2) {
-      console.log(num1 / num2);
+      return result = (num2 == 0) ? "can't divid by 0" : num1 / num2;
 }
 
-function operate(operator, num1, num2) {
-      let choixOperator = operator;
-      let firstNbr = num1;
-      let secondNbr = num2;
+// operating function : 
+function operate(num1, operator, num2) {
 
-      switch (choixOperator) {
+      switch (operator) {
             case '+':
-                  add(firstNbr, secondNbr);
+                  add(num1, num2);
                   break;
             case '-':
-                  subtract(firstNbr, secondNbr);
+                  subtract(num1, num2);
                   break;
             case '*':
-                  multiply(firstNbr, secondNbr);
+                  multiply(num1, num2);
                   break;
             case '/':
-                  divide(firstNbr, secondNbr);
+                  divide(num1, num2);
                   break;
       }
+      return result;
 }
 
-
+// Values from digit buttons : 
 function getNumber() {
       let digits = document.querySelector(".digits").childNodes;
       console.log(digits);
-      digits.forEach(nbr => {
-            if (nbr.innerText !== undefined) {
-                  nbr.addEventListener('click', populateScreen);
-            }
-      });
-}
-
-function getOperator() {
-      let operators = document.querySelector(".operators").childNodes;
-      console.log(operators);
-      operators.forEach(op => {
-            if (op.innerText !== undefined) {
-                  op.addEventListener('click', (e) => {
-                        let operator = e.target.innerText;
-                        console.log(operator);
-                        if (operator !== "=") {
-                              saveValue();
-                              populateScreen(e);
-                        };
-
+      digits.forEach(digit => {
+            if (digit.innerText !== undefined) {
+                  console.log(digit.innerText);
+                  let numbers = [];
+                  digit.addEventListener('click', (e) => {
+                        numbers.push(digit.innerText);
+                        populateScreen(e);
                   });
             }
       });
 }
-let screen = document.querySelector(".operation");
+
+// Values from operator buttons : 
+function getOperator() {
+      let operators = document.querySelector(".operators").childNodes;
+      console.log(operators);
+      operators.forEach(operator => {
+            if (operator.innerText !== undefined) {
+                  console.log(operator.innerText);
+                  let op = [];
+                  operator.addEventListener('click', (e) => {
+                        if (operator.innerText !== "=") {
+                              op.push(operator.innerText);
+                              populateScreen(e);
+                        } else {
+                              calcul();
+                        }
+                  });
+            }
+      });
+}
+
+// Values from option buttons : 
+
+function getOption() {
+      let options = document.querySelector(".options").childNodes;
+      console.log(options);
+      options.forEach(option => {
+            if (option.innerText !== undefined) {
+                  console.log(option.innerText);
+                  option.addEventListener('click', () => {
+                        switch (option.innerText) {
+                              case "Del":
+                                    deleteNumber();
+                                    break;
+                              case "AC":
+                                    clearScreen();
+                                    break;
+                        }
+                  });
+            }
+      });
+}
+
+// screen :
+let input = document.querySelector(".input");
+let output = document.querySelector(".output");
 
 function populateScreen(e) {
-      screen.innerText += e.target.innerText;
-      console.log(screen.innerText);
-
+      input.innerText += e.target.innerText;
+      console.log("valeur de l'input : " + input.innerText);
+      return inputValue = input.innerText;
 }
 
-function saveValue() {
-      let num1 = screen.innerText;
-      console.log(num1);
-
-      return num1;
+function clearScreen() {
+      let clear = document.querySelector(".options").childNodes;
+      clear.forEach(btn => {
+            if (btn.innerText !== undefined) {
+                  btn.addEventListener('click', () => {
+                        input.innerText = "";
+                        output.innerText = "";
+                  });
+            }
+      })
 }
+
+function deleteNumber() {
+
+      let inputList = Array.from(inputValue);
+      inputList.pop();
+      input.innerText = inputList.join("");
+}
+
+// calculator : 
+function calcul() {
+      /([0-9\.]+)([+*/-])([0-9\.]+)/g.exec(inputValue);
+      output.innerText += operate(parseFloat(RegExp.$1), RegExp.$2, parseFloat(RegExp.$3));
+}
+
+// init : 
+getNumber();
+getOperator();
+getOption();
